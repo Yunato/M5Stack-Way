f93662e0c8d78a1dc27d72f5c093915bfe4165b8
Yunato <yunato525@gmail.com>
2019-11-05 22:23:53 +0900

    Add onTimer that run per 4ms

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,75 @@
-#include <Arduino.h>
+#include <M5Stack.h>
+
+// LCD
+# define LCD_HEIGHT 240
+# define LCD_WIDTH  320
+
+// time
+# define SEC_FROM_MICRO   1000000
+# define SEC_FROM_MILLI   1000
+# define MILLI_FROM_MICRO 1000
+
+TaskHandle_t task1;
+// TaskHandle_t task2;
+
+volatile long t_count = -1;
+hw_timer_t *timer = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+
+void task1_fun(void *params) {
+  int count = 0;
+  while (1) {
+    delay(1);
+    ++count;
+
+    if (t_count > 2) {
+      timerAlarmDisable(timer);
+      M5.Lcd.printf("count  : %d\n", count);
+      M5.Lcd.printf("t_count: %ld\n", t_count);
+      count = 0;
+      t_count = -1;
+
+      delay(1000);
+
+      M5.Lcd.fillScreen(BLACK);
+      M5.Lcd.setCursor(0, 0);
+      timerAlarmEnable(timer);
+    }
+  }
+}
+
+// void task2_fun(void *params) {
+//   int count = 0;
+//   while (1) {
+//     ++count = 0;
+//     vTaskDelay(4);
+//   }
+// }
+
+void IRAM_ATTR onTimer() {
+  portENTER_CRITICAL_ISR(&timerMux);
+  ++t_count;
+  portEXIT_CRITICAL_ISR(&timerMux);
+}
+
 
 void setup() {
   // put your setup code here, to run once:
+
+  M5.begin();
+
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setTextColor(WHITE);
+  M5.Lcd.setTextSize(3);
+
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 0 * SEC_FROM_MICRO + 4 * MILLI_FROM_MICRO + 0, true);
+  timerAlarmEnable(timer);
+
+  // task function, task name, stack size, parameter, priority, task handle, task affinity
+  xTaskCreatePinnedToCore(task1_fun, "task1", 4096, NULL, 2, &task1, 0);
+  // xTaskCreatePinnedToCore(task2_fun, "task2", 4096, NULL, 1, &task2, 0);
 }
 
 void loop() {
